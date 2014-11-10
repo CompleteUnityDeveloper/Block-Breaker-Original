9c5b1c4b401aa9e520f346c227928b8961c27a24
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-10 17:12:34 +0000

    Import & Modify The Menu System

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}

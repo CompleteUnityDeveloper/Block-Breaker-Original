1e371fb0a85bd28a05ece9f513e75d826bf45384
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-11 15:04:41 +0000

    Playing Background Music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {		
+		GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

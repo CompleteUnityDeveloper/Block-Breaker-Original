eb19ce588b796d75e169fdc1efedc0d9e52c6331
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-11 15:34:01 +0000

    Introducing Static Variables

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,10 +2,18 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
 	
 	// Use this for initialization
-	void Start () {		
-		GameObject.DontDestroyOnLoad(gameObject);
+	void Start () {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		
 	}
 	
 	// Update is called once per frame

38dfe8917e525ed0b3a9029dc2383357218f45e3
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-14 18:20:20 +0000

    Colliders, Triggers & Collisions in Unity

---
+++ Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print ("Trigger");
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		print ("Collision");	
+	}
+	
+}

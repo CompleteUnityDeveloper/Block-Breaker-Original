71ef482c8806ed474c60ba492360964a2e14d7d1
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-17 13:47:58 +0000

    11 GameObject Movement By Mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+	
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		
+		this.transform.position = paddlePos;
+	}
+}

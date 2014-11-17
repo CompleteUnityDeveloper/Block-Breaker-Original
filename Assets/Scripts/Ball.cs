592c3d62eef2fc57280fc53888ad272fbb280c95
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-17 16:20:42 +0000

    Launching Ball On Mouse Click

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			// Lock the ball relative to the paddle.
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			// Wait for a mouse press to launch.
+			if (Input.GetMouseButtonDown(0)) {
+				print ("Mouse clicked, launch ball");
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+}

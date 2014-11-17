0ee14956ac909c6c54d42137ab21fbb8d1cdf956
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-17 10:37:10 +0000

    Tidying Up Before Moving On

--- Assets/Scripts/LooseCollider.cs
+++ Assets/Scripts/LooseCollider.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class LooseCollider : MonoBehaviour {
 
+	public LevelManager levelManager;
+	
 	void OnTriggerEnter2D (Collider2D trigger) {
 		print ("Trigger");
+		levelManager.LoadLevel("Win Screen");
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision) {

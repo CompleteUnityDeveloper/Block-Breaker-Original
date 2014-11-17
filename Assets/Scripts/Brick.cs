191881e209d0f7b35065bf079738efb8f99502fa
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-17 18:32:23 +0000

    Creating & Using Unity Prefabs

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+	public int maxHits;
+	
+	private int timesHit;
+	
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		timesHit++;
+	}
+}

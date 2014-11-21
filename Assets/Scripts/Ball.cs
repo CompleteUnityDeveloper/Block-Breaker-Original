a72fc12664a8616e605820617efddb0013532bf9
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-21 17:12:41 +0000

    Stop Boring Loops With Random.Range()

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -31,8 +31,11 @@ public class Ball : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D collision) {
 		// Ball does not trigger sound when brick is destoyed.
 		// Not 100% sure why, possibly because brick isn't there.
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+		
 		if (hasStarted) {	
 			audio.Play();
+			rigidbody2D.velocity += tweak;
 		}
 	}
 }

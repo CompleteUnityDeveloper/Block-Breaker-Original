42e950b20d735584836f52912eff7e341fe85971
Ben Tristem <ben@CompleteUnityDeveloper.com>
2014-11-21 17:49:44 +0000

    Automated Play Testing

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,19 +3,34 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
+	public bool autoPlay = false;
+
+	private Ball ball;
+	
 	void Start () {
-	
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
-		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		if (!autoPlay) {
+			MoveWithMouse();
+		} else {
+			AutoPlay();
+		}
+	}
 	
+	void AutoPlay() {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		Vector3 ballPos = ball.transform.position;
+		paddlePos.x = Mathf.Clamp(ballPos.x, 0.5f, 15.5f);
+		this.transform.position = paddlePos;
+	}
+	
+	void MoveWithMouse () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-		
 		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
-		
 		this.transform.position = paddlePos;
 	}
 }

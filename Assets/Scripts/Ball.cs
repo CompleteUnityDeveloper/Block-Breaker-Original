04731f6f5a7916742fe0f0cc11269722e20545f9
Ben Tristem <github@bentristem.com>
2018-04-25 21:48:05 +0100

    Refactored Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,41 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Ball : MonoBehaviour {
-	
-	private Paddle paddle;
-	private bool hasStarted = false;
-	private Vector3 paddleToBallVector;
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Vector2 launchVelocity = new Vector2(2f, 10f);
+    [Range(0,1f)][SerializeField] float randomFactor = 0.2f;
+
+	Paddle paddle;
+	bool hasStarted = false;
+	Vector3 paddleToBallVector;
 
 	// Use this for initialization
-	void Start () {
-		paddle = GameObject.FindObjectOfType<Paddle>();
+	void Start ()
+    {
+		paddle = FindObjectOfType<Paddle>();
 		paddleToBallVector = this.transform.position - paddle.transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!hasStarted) {
-			// Lock the ball relative to the paddle.
-			this.transform.position = paddle.transform.position + paddleToBallVector;
-			
-			// Wait for a mouse press to launch.
-			if (Input.GetMouseButtonDown(0)) {
-				print ("Mouse clicked, launch ball");
-				hasStarted = true;
-				this.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
-			}
-		}
-	}
-	
-	void OnCollisionEnter2D (Collision2D collision) {
-		// Ball does not trigger sound when brick is destoyed.
-		// Not 100% sure why, possibly because brick isn't there.
-		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
-		
-		if (hasStarted) {	
-			GetComponent<AudioSource>().Play();
-			GetComponent<Rigidbody2D>().velocity += tweak;
-		}
-	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(
+            Random.Range(0f, randomFactor),
+            Random.Range(0f, randomFactor)
+        );
+
+        if (hasStarted)
+        {
+            GetComponent<AudioSource>().Play();
+            GetComponent<Rigidbody2D>().velocity += velocityTweak;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        this.transform.position = paddle.transform.position + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        // Wait for a mouse press to launch.
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = launchVelocity;
+        }
+    }
 }
